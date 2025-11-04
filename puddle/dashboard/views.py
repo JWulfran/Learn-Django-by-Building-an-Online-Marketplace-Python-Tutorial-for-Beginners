a21102a387e5b16152883ffb218610940af9c9aa
Jean GAHOUSSOU <gahoussoujean6475@gmail.com>
2025-11-04 05:30:46 -0500

    Dashboarb

---
+++ puddle/dashboard/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from item.models import Item
+
+
+# Create your views here.
+
+@login_required
+def dashboard(request):
+    items = Item.objects.filter(creeated_by = request.user)
+
+    return render(request, 'dashboard/index.html', {
+        'items': items
+    })
